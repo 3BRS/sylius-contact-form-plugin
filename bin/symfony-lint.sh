0675ef20dc05b877a0d001822ee654bd1bfee9e4
Jaroslav Týc <mail@jaroslavtyc.com>
2024-01-08 09:31:09 +0100

    Add Behat config for old Sylius

--- bin/symfony-lint.sh
+++ bin/symfony-lint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 IFS=$'\n\t'
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 bin/console --no-interaction lint:yaml src
 bin/console --no-interaction lint:container
---
+++ tests/Application/src/Behat/DummyContext.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ThreeBRS\SyliusContactFormPlugin\Behat;
+
+use Behat\Behat\Context\Context;
+
+class DummyContext implements Context
+{
+
+}
